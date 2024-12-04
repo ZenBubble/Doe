95afd2f958498ef86f8e1ad28984cfc9ce45a90e
Dorson Tang <apersonnameddorson@gmail.com>
2024-12-03 18:01:20 -0800

    test

---
+++ build/doe_simulation/CMakeFiles/thruster_driver_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thruster_driver_plugin.dir/src/thruster_driver_plugin.cpp.o"
+  "CMakeFiles/thruster_driver_plugin.dir/src/thruster_driver_plugin.cpp.o.d"
+  "libthruster_driver_plugin.pdb"
+  "libthruster_driver_plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thruster_driver_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/doe_simulation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dorson/Documents/github/Doe/src/doe_simulation
+# Build directory: /home/dorson/Documents/github/Doe/build/doe_simulation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
